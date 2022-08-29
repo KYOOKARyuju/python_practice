ac4fbe9f16c138acbbe0ac97de05993ed78a8b84
ryuju kyooka <jingganglongshu@gmail.com>
2022-08-29 13:47:17 +0900

    initial commit

---
+++ k01.py
@@ -0,0 +1,5 @@
+
+import pandas as pd
+val = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
+df = pd.DataFrame(val, index = [0, 2, 4], columns = list('def'))
+df
---
+++ kaggle/titanic/taitanic.py
@@ -0,0 +1,21 @@
+#%%
+import pandas as pd
+import numpy as numpy
+
+train_data= pd.read_csv("train.csv")
+train_data = pd.DataFrame(train_data)
+train_data
+# %%
+test_data= pd.read_csv("test.csv")
+test_data = pd.DataFrame(test_data)
+test_data
+# %%
+train_data.info()
+#%%
+test_data.info()
+# %%
+#nullの数を調べる
+train_data["Age"].isnull().sum()
+
+#nullの割合を調べる
+# %%
